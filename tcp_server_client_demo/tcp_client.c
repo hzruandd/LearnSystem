e5b12d3ac67ab3657a3633aac0020884fe2b8dcd
hzruandd <82433422@qq.com>
2016-03-28 11:47:43 +0800

    add tcp_server_client_demo

---
+++ tcp_server_client_demo/tcp_client.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <netinet/in.h>  
+#include <fcntl.h>
+#include <strings.h>
+
+int socket_init();
+void socket_deinit();
+void socket_connect(int fd, char* ip, int port);
+
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        printf("Usage: %s <Server IP> <Server Port>\n", argv[0]);
+        return -1;
+    }
+
+    int sockfd = socket_init();
+    socket_connect(sockfd, argv[1], atoi(argv[2]));
+    printf("Connected to %s\n", argv[1]);
+
+    char buf[1024] = {0};
+    int len = recv(sockfd, buf, sizeof(buf), 0);
+    if (len >= 0) {
+        buf[len] = '\0';
+    }
+    printf("RECV: %s\n", buf);
+
+    socket_deinit(sockfd);
+    return 0;
+}
+
+void socket_connect(int fd, char* ip, int port) {
+    struct sockaddr_in addr;
+    socklen_t addrlen = sizeof(addr);
+
+    bzero(&addr, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    addr.sin_addr.s_addr = inet_addr(ip);
+
+    if (connect(fd, (struct sockaddr*)&addr, addrlen) < 0) {
+        perror("socket connect failed");
+        exit(-1);
+    }
+}
+
+int socket_init() {
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (fd < 0) {
+        perror("socket create failed.");
+        exit(-1);
+    }
+
+    return fd;
+}
+
+void socket_deinit(int fd) {
+    if (fd >= 0) close(fd);
+}
+
+
